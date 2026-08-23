23fd3cbf98805b90247e2cbb92aef0a55d7304de
meggy kim <jihyunkims495@gmail.com>
2026-08-23 19:48:50 +0900

    0823:implement specialized book models

--- 02_PYTHON/python_final_project_김지현/models/specialized_books.py
+++ 02_PYTHON/python_final_project_김지현/models/specialized_books.py
@@ -6,14 +6,46 @@ from models.base_book import Book
 class PrintedBook(Book):
     """일반 단행본 클래스."""
 
-    # TODO: Book을 상속받고 필요한 추가 속성을 정의한다.
-    # TODO: 상세 정보 메서드를 오버라이딩한다.
-    pass
+    def __init__(self, title, author, isbn, pages):
+        super().__init__(title, author, isbn)
+        self.__pages = pages
+
+    @property
+    def pages(self):
+        return self.__pages
+
+    def to_dict(self):
+        info = super().to_dict()
+        info.update({"유형": "일반 단행본", "페이지": self.__pages})
+        return info
+
+    def show_info(self):
+        status = "대여 중" if self.is_rented else "대여 가능"
+        return (
+            f"[일반 단행본] {self.title} | 저자: {self.author} | "
+            f"ISBN: {self.isbn} | {self.__pages}쪽 | {status}"
+        )
 
 
 class Ebook(Book):
     """전자도서 클래스."""
 
-    # TODO: Book을 상속받고 전자도서만의 추가 속성을 정의한다.
-    # TODO: 상세 정보 메서드를 오버라이딩한다.
-    pass
+    def __init__(self, title, author, isbn, file_format):
+        super().__init__(title, author, isbn)
+        self.__file_format = file_format
+
+    @property
+    def file_format(self):
+        return self.__file_format
+
+    def to_dict(self):
+        info = super().to_dict()
+        info.update({"유형": "전자도서", "파일형식": self.__file_format})
+        return info
+
+    def show_info(self):
+        status = "대여 중" if self.is_rented else "대여 가능"
+        return (
+            f"[전자도서] {self.title} | 저자: {self.author} | "
+            f"ISBN: {self.isbn} | 형식: {self.__file_format} | {status}"
+        )
