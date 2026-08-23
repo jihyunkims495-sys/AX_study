36d6823813737181a9909b20ce0772c4b2d417b0
meggy kim <jihyunkims495@gmail.com>
2026-08-23 16:43:46 +0900

    0823:setup project helper functions

---
+++ 02_PYTHON/python_final_project_김지현/utils/helpers.py
@@ -0,0 +1,19 @@
+"""입력 검증 및 포맷팅 등 공통 함수 모듈."""
+
+
+def is_blank(value):
+    """공백 입력 여부를 검사하기 위한 함수 뼈대."""
+    # TODO: 문자열이 비어 있거나 공백뿐인지 검사한다.
+    raise NotImplementedError
+
+
+def get_menu_choice():
+    """메뉴 선택 입력과 예외 처리를 담당할 함수 뼈대."""
+    # TODO: input()과 try-except를 사용해 올바른 메뉴 번호만 반환한다.
+    raise NotImplementedError
+
+
+def validate_isbn(isbn, isbn_set):
+    """ISBN 유효성과 중복 여부를 검사할 함수 뼈대."""
+    # TODO: 공백/중복 ISBN을 차단한다.
+    raise NotImplementedError
